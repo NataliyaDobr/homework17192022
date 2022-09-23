f07a0d8f49e071ca04301807c9af57e5345bae86
NataliyaDobr <n.m.suvorova@mail.ru>
2022-09-23 10:38:32 +0300

    task2 part2

--- homework.py
+++ homework.py
@@ -8,6 +8,7 @@
 # Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
+# Первая часть
 # import codecs
 # f = codecs.open('game.txt','w', "utf-8")
 # cnt_player = 2 # количество игроков
@@ -40,10 +41,115 @@
 #         step2 = randint(1,28)
 #         f.write('второй забирает - '+ str(step2)+ ' конфет' "\n")
 #         cnt_candies = cnt_candies-step2
-# print(winner)
 # if winner == first_player:
 #     f.write('Победил first_player')
 # else: 
 #     f.write('Победил second_player')
 # f.close()
+# Вторая часть игра с ботом
+# import codecs
 
+# f = codecs.open('game.txt','w', "utf-8")
+# cnt_player = 2 # количество игроков
+# cnt_candies = 2021
+
+# from random import randint
+
+# first_player = randint(1,2) # 1 - бот, 2 - вы
+# if first_player == 1:
+#     print('Первым ходит бот '  "\n")
+#     f.write('Первым ходит бот '  "\n")
+# else:
+#     print('Первым ходите вы' "\n")
+#     f.write('Первым ходите вы' "\n")
+
+# for i in range(1,cnt_candies):
+#      f.write(str(i) + ': осталось ')
+#      f.write(str(cnt_candies)+' конфет' "\n")
+#      print(str(i) + ': осталось '+str(cnt_candies)+' конфет' "\n")
+#      if first_player%2!=0:
+#        if cnt_candies<=28:
+#         print('выйграл бот')
+#         f.write('выйграл бот')
+#         break
+#        else: 
+#         step = randint(1,28)
+#         print('бот забирает - ' +str(step) +' конфет' "\n")
+#         f.write('бот забирает - ' +str(step) +' конфет' "\n")
+#         cnt_candies = cnt_candies-step
+#      else:
+#         if cnt_candies<=28:
+#          f.write('вы победитель')
+#          print('вы победитель')
+#          break
+#         else:
+#             step = input('сколько конфет вы возьмете? ')
+#             f.write('Вы забрали - '+ str(step)+ ' конфет' "\n")
+#             print('Вы забрали - '+ str(step)+ ' конфет' "\n")
+#             cnt_candies = cnt_candies - int(step)
+#      first_player +=1
+# f.close()
+
+# Создайте программу для игры в ""Крестики-нолики"".
+# from pickle import FALSE
+
+
+# field = list(range(1,10))
+
+# def draw_field(field):
+#     print("-"*13)
+#     for i in range(3):
+#         print('|',field[0+i*3],'|',field[1+i*3],'|',field[2+i*3],'|')
+#         print("-"*13)
+
+# def take_input(player_token):
+#     valid = False
+#     while not valid:
+#         player_answer = input("Куда ставим "+player_token+"?")
+#         try:
+#             player_answer = int(player_answer)
+#         except:
+#             print ("Некорректный ввод. Вы уверены, что ввели число?")
+#             continue
+#         if player_answer >= 1 and player_answer <= 9:
+#             if (str(field[player_answer-1]) not in "XO"):
+#                 field[player_answer-1] = player_token
+#                 valid = True
+#             else:
+#                 print ("Клетка занята")
+#         else:
+#             print ("Некорректный ввод. Введите число от 1 до 9")
+
+# def check_win(field):
+#     win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+#     for each in win_coord:
+#         if field[each[0]] == field[each[1]] == field[each[2]]:
+#             return field[each[0]]
+#     return False
+
+# def main(field):
+#     counter = 0
+#     win = False
+#     while not win:
+#         draw_field(field)
+#         if counter % 2 == 0:
+#             take_input("X")
+#         else:
+#             take_input("O")
+#         counter += 1
+#         if counter > 4:
+#             tmp = check_win(field)
+#             if tmp:
+#                 print (tmp, "выиграл!")
+#                 win = True
+#                 break
+#         if counter == 9:
+#             print ("Ничья")
+#             break
+#     draw_field(field)
+
+# main(field)
+
+# draw_field(field)
+
+# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
